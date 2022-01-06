ef0b20422fa1e69c898c53dd3e694d63cc429ee6
Pablo RDev <pabloriojod@gmail.com>
2022-01-06 20:27:57 +0100

    FETCH

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  interface post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  }
+
+  const [postList, setPostList] = useState<post[]>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json: post[]) => setPostList(json));
+  }, []);
+
+  console.log(postList);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {postList.map((post) => (
+        <div>
+          <div>
+            <li>UserID: {post.userId}</li>
+            <li>ID: {post.id}</li>
+            <li>Title: {post.title}</li>
+            <li>Body: {post.body}</li>
+          </div>
+          <button type="button">Delete</button>
+          <hr />
+        </div>
+      ))}
+    </>
   );
 }
 
